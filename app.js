cf9facb0e1f3c609929e8863186d51fad2aeaa89
Andrew Birch <ab8218-cp@yahoo.com>
2025-11-28 14:59:02 +0000

    BUILD: completed buttons and grid css

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //TODO: Create thumbnail images, so the user can click on them and see them in a fullscreen format
-//STEP 1: store image data
 
+//STEP 1: store image data
 const imageData = [
     {
         imageName: "img-one",
@@ -24,34 +24,28 @@ const thumbContainer = document.getElementById("thumbnail-container");
 
 //STEP 2: create thumbnail images
 function createThumbnails() {
-    for (i = 0; i < imageData.length; i++) {
+    for (let i = 0; i < imageData.length; i++) {
         const img = document.createElement("img");
         img.src = imageData[i].imageSrc;
         img.alt = imageData[i].imageAlt;
         img.id = imageData[i].imageName;
         img.className = "ri";
         img.title = imageData[i].imageAlt;
-        img.addEventListener("click", createFullscreenImage);
+        img.addEventListener("click", () => { createFullscreenImage(i); });
         thumbContainer.appendChild(img);
     }
  }
    
 
-    //STEP 3: create fullscreen images
-    function createFullscreenImage() {
-        fullScreenContainer.innerHTML = ""; // remove image
-        //create image
-        //update its values (properties)
-        //add className for styling (making the image full screen) 
-        const img = document.createElement("img");
-        img.src = imageData.imageSrc;
-        img.alt = imageData.imageAlt;
-        //append the image to the container
-        // fullScreenContainer.appendChild(img);
+   //STEP 3: create fullscreen images
+   function createFullscreenImage(selected) {
+        fullScreenContainer.innerHTML = "";
+        const fullImg = document.createElement("img");
+        fullImg.src = imageData[selected].imageSrc;
+        fullImg.alt = imageData[selected].imageAlt;
+        fullImg.className = "ri";
+        fullScreenContainer.appendChild(fullImg);
     }
-
-//add the createFullscreenImages function as the event handler of the event above
-//call the createThumbnails function
 
 createThumbnails();
 
