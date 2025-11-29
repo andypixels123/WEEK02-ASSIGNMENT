9af0fa2c4063a9420d480763c97f8665299eb58c
Andrew Birch <ab8218-cp@yahoo.com>
2025-11-29 00:27:41 +0000

    BUILD: completed responsive image gallery

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # WEEK02-ASSIGNMENT
 Responsive Image Gallery
+----------------------------
 
 # Reflection
 
@@ -16,3 +17,14 @@ Requesting feedback about a specific part of your submission.
 What useful external sources helped you complete the assignment (e.g Youtube tutorials)?
 What errors or bugs did you encounter while completing your assignment? How did you solve them?
 What went really well and what could have gone better?
+
+
+REFLECTION
+--------------------
+I believe I have completed all requirements. I have created a image gallery with wrap-around navigation in forwards and backwards directions as an addition to the brief. Also included are clickable thumbnails with event handlers attached. All images are responsive and can be viewed on both desktop and mobile devices. The main image is set to 100% width with a small margin surround to improve aesthetics. Thumbnails and forward/backward navigation buttons fall below the main image at screen widths of 800px width and smaller by utilising an @media query. All images have 'alt' tags included.
+
+I used a css grid layout which was a bit problematic to code correctly, I acheived this with some help from Manuel.
+
+I used a book entitled 'Javascript & Ajax' Visual Quickstart Guide for ideas and inspiration concerning the forwards and backwards navigation buttons logic which I created as an added extra to this assignment as it was not a requirement but improves functionality.
+
+I also used W3 Schools / MDN websites for reference regarding css grid.
--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 //TODO: Create thumbnail images, so the user can click on them and see them in a fullscreen format
 
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
 const fullScreenContainer = document.getElementById("fullscreen-container");
 const thumbContainer = document.getElementById("thumbnail-container");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+prevBtn.addEventListener("click", showPrev);
+nextBtn.addEventListener("click", showNext);
+let currentImg = 0;
 
 //STEP 1: store image data
 const imageData = [
     {
         imageName: "img-one",
         imageSrc: "./images/image-one.jpg",
-        imageAlt: "alt text",
+        imageAlt: "mountain lake"
     },
     {
         imageName: "img-two",
         imageSrc: "./images/image-two.jpg",
-        imageAlt: "alt text",
+        imageAlt: "misty woodland"
     },
     {
         imageName: "img-three",
         imageSrc: "./images/image-three.jpg",
-        imageAlt: "alt text",
+        imageAlt: "mountain pass",
     },
 ];
 
+const objL = imageData.length;
+
 //STEP 2: create thumbnail images
 function createThumbnails() {
-    for (let i = 0; i < imageData.length; i++) {
+    for (let i = 0; i < objL; i++) {
         const img = document.createElement("img");
         img.src = imageData[i].imageSrc;
         img.alt = imageData[i].imageAlt;
@@ -36,31 +41,33 @@ function createThumbnails() {
         img.addEventListener("click", () => { createFullscreenImage(i); });
         thumbContainer.appendChild(img);
     }
- }
-   
+}
 
-   //STEP 3: create fullscreen images
-   function createFullscreenImage(selected) {
-        fullScreenContainer.innerHTML = "";
-        const fullImg = document.createElement("img");
-        fullImg.src = imageData[selected].imageSrc;
-        fullImg.alt = imageData[selected].imageAlt;
-        fullImg.className = "ri";
-        fullScreenContainer.appendChild(fullImg);
-        adVance(selected);
-    }
+//STEP 3: create fullscreen images
+function createFullscreenImage(i) {
+    fullScreenContainer.innerHTML = "";
+    const fullImg = document.createElement("img");
+    fullImg.src = imageData[i].imageSrc;
+    fullImg.alt = imageData[i].imageAlt;
+    fullImg.className = "ri";
+    fullScreenContainer.appendChild(fullImg);
+}
 
-    function adVance(x) {
-        let prev = x - 1;
-        // console.log(prev);
-        if(prev < 0) { prev = imageData.length; }
-        let next = x + 1;
-        if(next > (imageData.length - 1)) { next = 0; }
-        prevBtn.removeEventListener("click", createFullscreenImage);
-        nextBtn.removeEventListener("click", createFullscreenImage);
-        prevBtn.addEventListener("click", () => { createFullscreenImage(prev); });
-        nextBtn.addEventListener("click", () => { createFullscreenImage(next); });
+function showPrev() {
+    if (currentImg === 0) { // wrap to last image in object 
+        currentImg = objL;
     }
+    currentImg--;
+    createFullscreenImage(currentImg);
+}
+
+function showNext() {
+    currentImg++;
+    if (currentImg === objL) { // wrap to first image in object
+        currentImg = 0;
+    }
+    createFullscreenImage(currentImg);
+}
 
 createThumbnails();
-createFullscreenImage(0);
+createFullscreenImage(currentImg);
